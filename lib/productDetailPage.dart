ec0ec7f2178b731045d23d2f94c64ea021ca002e
erdemgocen44 <erdemgocen44@gmail.com>
2021-05-27 18:13:50 +0300

    depişim

--- lib/productDetailPage.dart
+++ lib/productDetailPage.dart
@@ -7,116 +7,238 @@ class ProductDetailPage extends StatefulWidget {
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
+  int _counter = 0;
+
+  void arttir() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void azalt() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 1,
-                  height: MediaQuery.of(context).size.height * 0.4,
+      backgroundColor: Color(0xffCB6C7F),
+      body: Column(
+        children: [
+          Stack(
+            clipBehavior: Clip.hardEdge,
+            fit: StackFit.loose,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: BoxDecoration(
+                  color: Color(0xffCB6C7F),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 200,
+                    ),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          buildDefaultText('Deals of the week', 11,
+                              FontWeight.bold, Colors.white),
+                          buildDefaultText('Fresh Strawberry ', 19,
+                              FontWeight.w400, Colors.white),
+                          buildDefaultText(
+                              'Milkshakes ', 23, FontWeight.bold, Colors.white),
+                          Row(
+                            children: [
+                              buildIconStars(),
+                              buildIconStars(),
+                              buildIconStars(),
+                              buildIconStars(),
+                              buildIconStars(),
+                            ],
+                          ),
+                          buildDefaultText(
+                            '125 Votes',
+                            10,
+                            FontWeight.w400,
+                            Colors.white,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                  left: 25,
+                  top: 50,
+                  child: Image.asset('assets/images/icecek.png')),
+              Positioned(
+                top: 190,
+                left: 220,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.08,
                   decoration: BoxDecoration(
-                    color: Color(0xffCB6C7F),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    ),
+                    color: Color(0xffB13E55),
                   ),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      SizedBox(
-                        width: 220,
+                      buildDefaultText(
+                        '£ 12',
+                        23,
+                        FontWeight.w400,
+                        Colors.white,
                       ),
-                      Container(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: 10,
-                            ),
-                            buildDefaultText('Deals of the week', 11,
-                                FontWeight.bold, Colors.white),
-                            buildDefaultText('Fresh Strawberry ', 19,
-                                FontWeight.w400, Colors.white),
-                            buildDefaultText('Milkshakes ', 23, FontWeight.bold,
-                                Colors.white),
-                            Row(
-                              children: [
-                                buildIconStars(),
-                                buildIconStars(),
-                                buildIconStars(),
-                                buildIconStars(),
-                                buildIconStars(),
-                              ],
-                            ),
-                            buildDefaultText(
-                              '125 Votes',
-                              10,
-                              FontWeight.w400,
-                              Colors.white,
-                            ),
-                            SizedBox(
-                              height: 80,
-                            ),
-                          ],
-                        ),
+                      buildDefaultText(
+                        '£ 19',
+                        23,
+                        FontWeight.w400,
+                        Color(0xffCB6C7F),
                       ),
                     ],
                   ),
                 ),
-                Positioned(
-                  top: 250,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 1,
-                    height: MediaQuery.of(context).size.height * 0.7,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                      ),
-                      color: Colors.white,
+              ),
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 1,
+            height: MediaQuery.of(context).size.height * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+              ),
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    buildDefaultText(
+                      'Materials',
+                      15,
+                      FontWeight.bold,
+                      Color(0xff424242),
                     ),
-                  ),
-                ),
-                Positioned(
-                    left: 25,
-                    top: 60,
-                    child: Image.asset('assets/images/icecek.png')),
-                Positioned(
-                  top: 220,
-                  left: 220,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    height: MediaQuery.of(context).size.height * 0.08,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                      ),
-                      color: Color(0xffB13E55),
+                    buildDefaultText(
+                      'Ice cream, heavy cream, milk, vanilla and\nstrawberries in a large.',
+                      12,
+                      FontWeight.w400,
+                      Color(0xff424242),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    buildDefaultText(
+                      'Instructions',
+                      15,
+                      FontWeight.bold,
+                      Color(0xff424242),
+                    ),
+                    buildDefaultText(
+                      'It is a long established fact that a reader will be\ndistracted by the readable content of a page when\nlooking at its layout. ',
+                      12,
+                      FontWeight.w400,
+                      Color(0xff424242),
+                    ),
+                    buildDefaultText(
+                      'Description',
+                      15,
+                      FontWeight.bold,
+                      Color(0xff424242),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        buildDefaultText(
-                          '£ 12',
-                          23,
-                          FontWeight.w400,
-                          Colors.white,
+                        Column(
+                          children: [
+                            Image.asset(
+                              'assets/images/ates.png',
+                            ),
+                            buildDefaultText(
+                              '4 Pieces',
+                              10,
+                              FontWeight.w400,
+                              Color(0xff474749),
+                            ),
+                          ],
                         ),
-                        buildDefaultText(
-                          '£ 19',
-                          23,
-                          FontWeight.w400,
-                          Color(0xffCB6C7F),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Column(
+                          children: [
+                            Image.asset(
+                              'assets/images/dondurma.png',
+                            ),
+                            buildDefaultText(
+                              '373 Calories',
+                              10,
+                              FontWeight.w400,
+                              Color(0xff474749),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildDefaultText(
+                            'Quantity',
+                            15,
+                            FontWeight.bold,
+                            Color(0xff424242),
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: azalt,
+                                icon: Icon(
+                                  Icons.remove_circle,
+                                  size: 30,
+                                  color: Color(0xffB0EAFD),
+                                ),
+                              ),
+                              Text('$_counter'),
+                              IconButton(
+                                onPressed: arttir,
+                                icon: Icon(
+                                  Icons.add_box_sharp,
+                                  size: 30,
+                                  color: Color(0xffB0EAFD),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
