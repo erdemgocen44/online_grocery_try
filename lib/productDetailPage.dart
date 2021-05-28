49e6603bf19e75c39be259a46ad0eb04dc9beaf6
erdemgocen44 <erdemgocen44@gmail.com>
2021-05-28 20:36:34 +0300

    TabBar ve Sliver tamam gibi

--- lib/productDetailPage.dart
+++ lib/productDetailPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:online_grocery_try/defaultText.dart';
+import 'package:online_grocery_try/store/storePage.dart';
 
 class ProductDetailPage extends StatefulWidget {
   @override
@@ -25,220 +26,284 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffCB6C7F),
-      body: Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.hardEdge,
-            fit: StackFit.loose,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 1,
-                height: MediaQuery.of(context).size.height * 0.3,
-                decoration: BoxDecoration(
-                  color: Color(0xffCB6C7F),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.hardEdge,
+              fit: StackFit.loose,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  decoration: BoxDecoration(
+                    color: Color(0xffCB6C7F),
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            buildDefaultText('Deals of the week', 11,
+                                FontWeight.bold, Colors.white),
+                            buildDefaultText('Fresh Strawberry ', 19,
+                                FontWeight.w400, Colors.white),
+                            buildDefaultText('Milkshakes ', 23, FontWeight.bold,
+                                Colors.white),
+                            Row(
+                              children: [
+                                buildIconStars(),
+                                buildIconStars(),
+                                buildIconStars(),
+                                buildIconStars(),
+                                buildIconStars(),
+                              ],
+                            ),
+                            buildDefaultText(
+                              '125 Votes',
+                              10,
+                              FontWeight.w400,
+                              Colors.white,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 200,
+                Positioned(
+                    left: 25,
+                    top: 30,
+                    child: Image.asset('assets/images/icecek.png')),
+                Positioned(
+                  top: 190,
+                  left: 220,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                      ),
+                      color: Color(0xffB13E55),
                     ),
-                    Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        buildDefaultText(
+                          '£ 12',
+                          23,
+                          FontWeight.w400,
+                          Colors.white,
+                        ),
+                        buildDefaultText(
+                          '£ 19',
+                          23,
+                          FontWeight.w400,
+                          Color(0xffCB6C7F),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.7,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                ),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      buildDefaultText(
+                        'Materials',
+                        15,
+                        FontWeight.bold,
+                        Color(0xff424242),
+                      ),
+                      buildDefaultText(
+                        'Ice cream, heavy cream, milk, vanilla and\nstrawberries in a large.',
+                        12,
+                        FontWeight.w400,
+                        Color(0xff424242),
+                      ),
+                      buildDefaultText(
+                        'Instructions',
+                        15,
+                        FontWeight.bold,
+                        Color(0xff424242),
+                      ),
+                      buildDefaultText(
+                        'It is a long established fact that a reader will be\ndistracted by the readable content of a page when\nlooking at its layout. ',
+                        12,
+                        FontWeight.w400,
+                        Color(0xff424242),
+                      ),
+                      buildDefaultText(
+                        'Description',
+                        15,
+                        FontWeight.bold,
+                        Color(0xff424242),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          SizedBox(
-                            height: 10,
-                          ),
-                          buildDefaultText('Deals of the week', 11,
-                              FontWeight.bold, Colors.white),
-                          buildDefaultText('Fresh Strawberry ', 19,
-                              FontWeight.w400, Colors.white),
-                          buildDefaultText(
-                              'Milkshakes ', 23, FontWeight.bold, Colors.white),
-                          Row(
+                          Column(
                             children: [
-                              buildIconStars(),
-                              buildIconStars(),
-                              buildIconStars(),
-                              buildIconStars(),
-                              buildIconStars(),
+                              Image.asset(
+                                'assets/images/ates.png',
+                              ),
+                              buildDefaultText(
+                                '4 Pieces',
+                                10,
+                                FontWeight.w400,
+                                Color(0xff474749),
+                              ),
                             ],
                           ),
-                          buildDefaultText(
-                            '125 Votes',
-                            10,
-                            FontWeight.w400,
-                            Colors.white,
-                          ),
                           SizedBox(
-                            height: 20,
+                            width: 30,
+                          ),
+                          Column(
+                            children: [
+                              Image.asset(
+                                'assets/images/dondurma.png',
+                              ),
+                              buildDefaultText(
+                                '373 Calories',
+                                10,
+                                FontWeight.w400,
+                                Color(0xff474749),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                  left: 25,
-                  top: 50,
-                  child: Image.asset('assets/images/icecek.png')),
-              Positioned(
-                top: 190,
-                left: 220,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomLeft: Radius.circular(20),
-                    ),
-                    color: Color(0xffB13E55),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      buildDefaultText(
-                        '£ 12',
-                        23,
-                        FontWeight.w400,
-                        Colors.white,
-                      ),
-                      buildDefaultText(
-                        '£ 19',
-                        23,
-                        FontWeight.w400,
-                        Color(0xffCB6C7F),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            buildDefaultText(
+                              'Quantity',
+                              15,
+                              FontWeight.bold,
+                              Color(0xff424242),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: azalt,
+                                  icon: Icon(
+                                    Icons.remove_circle,
+                                    size: 30,
+                                    color: Color(0xffB0EAFD),
+                                  ),
+                                ),
+                                Text('$_counter'),
+                                IconButton(
+                                  onPressed: arttir,
+                                  icon: Icon(
+                                    Icons.add_box_sharp,
+                                    size: 30,
+                                    color: Color(0xffB0EAFD),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 1,
-            height: MediaQuery.of(context).size.height * 0.7,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(50),
-              ),
-              color: Colors.white,
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    buildDefaultText(
-                      'Materials',
-                      15,
-                      FontWeight.bold,
-                      Color(0xff424242),
-                    ),
-                    buildDefaultText(
-                      'Ice cream, heavy cream, milk, vanilla and\nstrawberries in a large.',
-                      12,
-                      FontWeight.w400,
-                      Color(0xff424242),
-                    ),
-                    buildDefaultText(
-                      'Instructions',
-                      15,
-                      FontWeight.bold,
-                      Color(0xff424242),
-                    ),
-                    buildDefaultText(
-                      'It is a long established fact that a reader will be\ndistracted by the readable content of a page when\nlooking at its layout. ',
-                      12,
-                      FontWeight.w400,
-                      Color(0xff424242),
-                    ),
-                    buildDefaultText(
-                      'Description',
-                      15,
-                      FontWeight.bold,
-                      Color(0xff424242),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Column(
-                          children: [
-                            Image.asset(
-                              'assets/images/ates.png',
-                            ),
-                            buildDefaultText(
-                              '4 Pieces',
-                              10,
-                              FontWeight.w400,
-                              Color(0xff474749),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: 30,
-                        ),
-                        Column(
-                          children: [
-                            Image.asset(
-                              'assets/images/dondurma.png',
-                            ),
-                            buildDefaultText(
-                              '373 Calories',
-                              10,
-                              FontWeight.w400,
-                              Color(0xff474749),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          buildDefaultText(
-                            'Quantity',
-                            15,
-                            FontWeight.bold,
-                            Color(0xff424242),
-                          ),
-                          Row(
-                            children: [
-                              IconButton(
-                                onPressed: azalt,
-                                icon: Icon(
-                                  Icons.remove_circle,
-                                  size: 30,
-                                  color: Color(0xffB0EAFD),
-                                ),
-                              ),
-                              Text('$_counter'),
-                              IconButton(
-                                onPressed: arttir,
-                                icon: Icon(
-                                  Icons.add_box_sharp,
-                                  size: 30,
-                                  color: Color(0xffB0EAFD),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.15,
+              decoration: BoxDecoration(
+                color: Color(0xffF8F8F8),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      buildDefaultText(
+                        '2 Items',
+                        9,
+                        FontWeight.w300,
+                        Color(0xff2B3D54),
                       ),
-                    ),
-                  ],
-                ),
+                      buildDefaultText(
+                        'Total:',
+                        17,
+                        FontWeight.w300,
+                        Color(0xff2B3D54),
+                      ),
+                      buildDefaultText(
+                        '£ 25',
+                        24,
+                        FontWeight.w300,
+                        Color(0xffB13E55),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.56,
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      decoration: BoxDecoration(
+                        color: Color(0xffB13E55),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StorePage(),
+                            ),
+                          );
+                        },
+                        child: buildDefaultText(
+                          'PURCHASE',
+                          17,
+                          FontWeight.w500,
+                          Colors.white,
+                        ),
+                      )),
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/store/storePage.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:online_grocery_try/defaultText.dart';
+import 'package:online_grocery_try/store/infoPage.dart';
+import 'package:online_grocery_try/store/menuPage.dart';
+import 'package:online_grocery_try/store/reviewPage.dart';
+
+class StorePage extends StatefulWidget {
+  @override
+  _StorePageState createState() => _StorePageState();
+}
+
+class _StorePageState extends State<StorePage>
+    with SingleTickerProviderStateMixin {
+  final double _minValue = 8.0;
+  late TabController _controller = TabController(
+    length: 3,
+    vsync: this,
+  );
+  int _selectedIndex = 0;
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+
+    _controller.addListener(
+      () {
+        setState(() {
+          _selectedIndex = _controller.index;
+        });
+        print(
+          "Selected Index: " + _controller.index.toString(),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 3,
+        child: SafeArea(
+          child: NestedScrollView(
+            body: TabBarView(
+              controller: _controller,
+              children: [
+                InfoPage(),
+                MenuPage(),
+                ReviewPage(),
+              ],
+            ),
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) => [
+              SliverAppBar(
+                expandedHeight: 170,
+                pinned: true,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.network('https://picsum.photos/500/200'),
+                  centerTitle: false,
+                ),
+                backgroundColor: Colors.grey[100],
+                elevation: 12.0,
+                bottom: TabBar(
+                  controller: _controller,
+                  labelPadding: EdgeInsets.all(0),
+                  indicatorPadding: EdgeInsets.all(0),
+                  unselectedLabelColor: Color(0xff5f6368),
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5), // Creates border
+                    color: Color(
+                      0xffA22E3A,
+                    ),
+                  ),
+                  indicatorColor: Colors.transparent,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorWeight: 3,
+                  tabs: [
+                    Container(
+                      decoration: BoxDecoration(),
+                      width: MediaQuery.of(context).size.width * 0.35,
+                      child: Tab(
+                        child: buildDefaultText(
+                          'Info',
+                          16,
+                          FontWeight.w400,
+                          Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.30,
+                      decoration: BoxDecoration(),
+                      child: Tab(
+                        child: buildDefaultText(
+                          'Menu',
+                          16,
+                          FontWeight.w400,
+                          Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.35,
+                      decoration: BoxDecoration(),
+                      child: Tab(
+                        child: buildDefaultText(
+                          'Reviews',
+                          16,
+                          FontWeight.w400,
+                          Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
